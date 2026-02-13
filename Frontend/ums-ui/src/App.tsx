a5eadf5456f6d64750f78a725a9d028eabdfc52e
ritesh-thakur-ums <rsthakur7@gmail.com>
2026-02-13 08:02:14 +0530

    Implemented ProtectedRoute for dashboard page access

--- Frontend/ums-ui/src/App.tsx
+++ Frontend/ums-ui/src/App.tsx
@@ -1,12 +1,18 @@
 import {Routes, Route, Navigate} from "react-router-dom"
 import Login from "./pages/Login";
 import AdminDashboard from "./pages/AdminDashboard";
+import ProtectedRoute from "./routes/ProtectedRoute";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element = {<Login/>} />
-      <Route path="/admin/dashboard" element={<AdminDashboard/>} />
+      <Route path="/admin/dashboard" element={
+        <ProtectedRoute>
+          <AdminDashboard/>
+        </ProtectedRoute>
+      }
+      />
       <Route path="*" element={<Navigate to="/login"/>} />
     </Routes>
   );
--- Frontend/ums-ui/src/pages/AdminDashboard.tsx
+++ Frontend/ums-ui/src/pages/AdminDashboard.tsx
@@ -1,10 +1,23 @@
+import { useNavigate } from "react-router-dom";
+
 const AdminDashboard = () => {
+   const navigate = useNavigate();
    const email = localStorage.getItem("email");
+
+   const handleLogout = () => {
+      localStorage.removeItem("token");
+      localStorage.removeItem("email");
+      localStorage.removeItem("roles");
+
+      navigate("/login");
+   };
 
    return(
        <div style = {{padding : "30px" }}>
           <h2>Admin Dashboard</h2>
           <p>Welcome, {email}</p>
+
+          <button onClick={handleLogout}>Logout</button>
        </div>
    );
 };
